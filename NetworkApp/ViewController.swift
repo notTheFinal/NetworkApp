1c0a5ffa2e77aa506d0b34f222679c1e7d575c89
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-06-09 16:15:41 +0800

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Александр on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
